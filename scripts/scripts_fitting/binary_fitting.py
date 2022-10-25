0eea0ca5d71082daff94b8907cb76a93660780ff
IsabHox <isabellehoxha@gmail.com>
2022-10-25 13:08:40 +0200

    disable paranoid, retry DWM

--- scripts/scripts_fitting/binary_fitting.py
+++ scripts/scripts_fitting/binary_fitting.py
@@ -26,6 +26,9 @@ from DWM import DWMTwoStimuli
 from extras import ICIntervalRatio, LossByMeans
 from utilities import process_binary
 
+import paranoid as pns
+pns.settings.Settings.set(enabled=False)
+
 #%% load data (this will be changed after upload of the dataset on Zenodo)
 data_file=pd.read_csv('../../data/Binary/RT_data.csv')#r'D:\PhD\data to share\RT_data.csv')
 subjects=np.unique(data_file.Subject)
@@ -106,24 +109,24 @@ for s in range (1): # (len(subjects)):
                       lossfunction=LossRobustLikelihood, verbose=False)
     
     #%%
-    # my_dwm=Model(name='my DWM', drift=DWMTwoStimuli(a=Fittable(minval=.1, maxval=10),
-    #                                              m0=Fittable(minval=.1, maxval=10),
-    #                                              m1=Fittable(minval=.1, maxval=10),
-    #                                              tau=Fittable(minval=.01, maxval=5)),
-    #             noise=NoiseConstant(noise=.3),
-    #             bound=BoundConstant(B=Fittable(minval=.1, maxval=5)),
-    #             IC=ICIntervalRatio(x0=Fittable(minval=-1, maxval=1), sz=Fittable(minval=0, maxval=1)),
-    #             overlay = OverlayChain(overlays=[OverlayNonDecisionUniform(nondectime=Fittable(minval = 0.1, maxval = 0.8), halfwidth=Fittable(minval=0., maxval=0.2)),
-    #                                              OverlayUniformMixture(umixturecoef=Fittable(minval=0, maxval=.1))]),
-    #             dx=0.005,
-    #             dt=0.005,
-    #             T_dur=2.0
-    #             )
+    my_dwm=Model(name='my DWM', drift=DWMTwoStimuli(a=Fittable(minval=.1, maxval=10),
+                                                  m0=Fittable(minval=.1, maxval=10),
+                                                  m1=Fittable(minval=.1, maxval=10),
+                                                  tau=Fittable(minval=.01, maxval=5)),
+                noise=NoiseConstant(noise=.3),
+                bound=BoundConstant(B=Fittable(minval=.1, maxval=5)),
+                IC=ICIntervalRatio(x0=Fittable(minval=-1, maxval=1), sz=Fittable(minval=0, maxval=1)),
+                overlay = OverlayChain(overlays=[OverlayNonDecisionUniform(nondectime=Fittable(minval = 0.1, maxval = 0.8), halfwidth=Fittable(minval=0., maxval=0.2)),
+                                                  OverlayUniformMixture(umixturecoef=Fittable(minval=0, maxval=.1))]),
+                dx=0.005,
+                dt=0.005,
+                T_dur=2.0
+                )
     
-    # fit_adjust_model(my_samples, my_dwm,
-    #                   fitting_method="differential_evolution",
-    #                   method="implicit",
-    #                   lossfunction=LossRobustLikelihood, verbose=False)
+    fit_adjust_model(my_samples, my_dwm,
+                      fitting_method="differential_evolution",
+                      method="implicit",
+                      lossfunction=LossRobustLikelihood, verbose=False)
     
     #%% some plotting to enjoy the result
     # plt.figure()
@@ -145,17 +148,17 @@ for s in range (1): # (len(subjects)):
     nl_loss=get_model_loss(my_nlddm, my_samples, lossfunction=LossRobustLikelihood)
     dm_loss=get_model_loss(my_ddm, my_samples, lossfunction=LossRobustLikelihood)
     ou_loss=get_model_loss(my_ou, my_samples, lossfunction=LossRobustLikelihood)
-    # dw_loss=get_model_loss(my_dwm, my_samples, lossfunction=LossRobustLikelihood)
+    dw_loss=get_model_loss(my_dwm, my_samples, lossfunction=LossRobustLikelihood)
     
     nl_bic=np.log(sample_size)*nparams_nl+2*nl_loss
     dm_bic=np.log(sample_size)*nparams_dm+2*dm_loss
     ou_bic=np.log(sample_size)*nparams_ou+2*ou_loss
-    # dw_bic=np.log(sample_size)*nparams_dw+2*dw_loss
+    dw_bic=np.log(sample_size)*nparams_dw+2*dw_loss
     
     nl_prediction_performance=get_model_loss(my_nlddm, my_samples, lossfunction=LossByMeans)
     dm_prediction_performance=get_model_loss(my_ddm, my_samples, lossfunction=LossByMeans)
     ou_prediction_performance=get_model_loss(my_ou, my_samples, lossfunction=LossByMeans)
-    # dw_prediction_performance=get_model_loss(my_dwm, my_samples, lossfunction=LossByMeans)
+    dw_prediction_performance=get_model_loss(my_dwm, my_samples, lossfunction=LossByMeans)
     
     #%% and save the results
     col_nlDDM=['Subject']
@@ -167,37 +170,37 @@ for s in range (1): # (len(subjects)):
         col_nlDDM.extend(my_nlddm.get_model_parameter_names())
         col_DDM.extend(my_ddm.get_model_parameter_names())
         col_OU.extend(my_ou.get_model_parameter_names())
-        # col_DWM.extend(my_dwm.get_model_parameter_names())
+        col_DWM.extend(my_dwm.get_model_parameter_names())
         nlddm_params=pd.DataFrame(columns=col_nlDDM)
         ddm_params= pd.DataFrame(columns=col_DDM)
         ou_params=pd.DataFrame(columns=col_OU)
-        # dwm_params=pd.DataFrame(columns=col_DWM)
+        dwm_params=pd.DataFrame(columns=col_DWM)
         performance=pd.DataFrame(columns=['Subject', 'LogLoss (nlDDM)','LogLoss (DDM)',
-                                          'LogLoss (OU)',#'LogLoss (DWM)',
-                                          'BIC (nlDDM)','BIC (DDM)','BIC (OU)',#'BIC (DWM)',
-                                          'Perf (nlDDM)','Perf (DDM)','Perf (OU)'])#, 'Perf (DWM)'])
+                                          'LogLoss (OU)','LogLoss (DWM)',
+                                          'BIC (nlDDM)','BIC (DDM)','BIC (OU)','BIC (DWM)',
+                                          'Perf (nlDDM)','Perf (DDM)','Perf (OU)', 'Perf (DWM)'])
     nlddm_dat, ddm_dat, ou_dat, dwm_dat=[subject], [subject], [subject], [subject]
     nlddm_dat.extend([k.default() for k in my_nlddm.get_model_parameters()])
     ddm_dat.extend([k.default() for k in my_ddm.get_model_parameters()])
     ou_dat.extend([k.default() for k in my_ou.get_model_parameters()])
-    # dwm_dat.extend([k.default() for k in my_dwm.get_model_parameters()])
+    dwm_dat.extend([k.default() for k in my_dwm.get_model_parameters()])
     
     nlddm_params.loc[len(nlddm_params.index)]=nlddm_dat
     ddm_params.loc[len(ddm_params.index)]=ddm_dat
     ou_params.loc[len(ou_params.index)]=ou_dat
-    # dwm_params.loc[len(dwm_params.index)]=dwm_dat
+    dwm_params.loc[len(dwm_params.index)]=dwm_dat
     
     performance.loc[len(performance.index)]=[subject, nl_loss, dm_loss, ou_loss, #dw_loss,
                                              nl_bic, dm_bic, ou_bic, #dw_bic,
                                               nl_prediction_performance,
                                               dm_prediction_performance,
-                                              ou_prediction_performance]#,
-                                              #dw_prediction_performance]
+                                              ou_prediction_performance,
+                                              dw_prediction_performance]
     
 #%%
 nlddm_params.to_csv('../../results/fitting_nlDDM_binary_IC.csv')
 ddm_params.to_csv('../../results/fitting_ddm_binary_IC.csv')
 ou_params.to_csv('../../results/fitting_ou_binary_IC.csv')
-# dwm_params.to_csv('../../results/fitting_dwm_binary_IC.csv')
+dwm_params.to_csv('../../results/fitting_dwm_binary_IC.csv')
 
 performance.to_csv('../../results/performance_IC.csv')
