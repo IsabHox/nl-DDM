b03d7d579e5c2c6a1f5b8ee8125e782b2926e405
IsabHox <isabellehoxha@gmail.com>
2022-12-02 11:40:45 +0100

    add behavioral analyses

--- scripts/scripts_behavior/process_Wagenmakers.py
+++ scripts/scripts_behavior/process_Wagenmakers.py
@@ -13,24 +13,42 @@ wgdata=pd.read_csv('../../data/Wagenmakers/SpeedAccData_filtered.csv').set_index
 wgdata=wgdata[wgdata.Subject!=2]
 
 #%% prepare settings for 
-result_df=pd.DataFrame(columns=['Subject', 'RT', 'Mean accuracy', 'Word type', 'Instruction', 'Time'])
 time_map={0:'Early', 1:'Late'}
 word_map={0:'non-existent', 1:'frequent' , 2:'rare', 3:'very rare'}
 inst_map={0:'accuracy', 1:'speed'}
 
 #%% and run
 subjects=np.unique(wgdata.Subject)
-
+cols=['Subject']
+for w in word_map:
+    for t in time_map:
+        for i in inst_map:
+            cols.append(f'RT {word_map[w]}, {time_map[t]}, {inst_map[i]}')
+            cols.append(f'Mean accuracy {word_map[w]}, {time_map[t]}, {inst_map[i]}')
+# result_df=pd.DataFrame(columns=['Subject', 'RT non-existent', 'RT easy', 'RT rare', 'RT very rare',
+#                                 'Mean accuracy non-existent', 'Mean accuracy frequent', 'Mean accuracy rare', 'Mean accuracy very rare', 
+#                                 'RT early', 'RT late', 
+#                                 'Mean accuracy early', 'Mean accuracy late', 
+#                                 'RT accuracy', 'RT speed', 
+#                                 'Instruction', 'Time'])
+result_df=pd.DataFrame(index=subjects, columns=cols)
 for subject in subjects:
     subdat=wgdata[wgdata.Subject==subject]
-    for w in np.unique(subdat.word_type):
-        wdat=subdat[subdat.word_type==w]
-        for i in np.unique(wdat.Condition):
-            idat=wdat[wdat.Condition==i]
-            for t in np.unique(idat.Late):
-                tdat=idat[idat.Late==t]
-                res=[subject,tdat['RT'].mean(), tdat['correct'].mean(),word_map[w],inst_map[i],time_map[t]]
-                result_df.loc[len(result_df)]=res
+    res=[subject]
+    res.extend(subdat.groupby(['word_type', 'Late','Condition'])['RT'].mean())
+    res.extend(subdat.groupby(['word_type', 'Late','Condition'])['correct'].mean())
+    # res.extend(subdat.groupby('Late')['RT'].mean())
+    # res.extend(subdat.groupby('Late')['correct'].mean())
+    # res.extend(subdat.groupby('Condition')['RT'].mean())
+    # res.extend(subdat.groupby('Condition')['correct'].mean())
+    # for w in np.unique(subdat.word_type):
+    #     wdat=subdat[subdat.word_type==w]
+    #     for i in np.unique(wdat.Condition):
+    #         idat=wdat[wdat.Condition==i]
+    #         for t in np.unique(idat.Late):
+    #             tdat=idat[idat.Late==t]
+    #             res=[subject,tdat['RT'].mean(), tdat['correct'].mean(),word_map[w],inst_map[i],time_map[t]]
+    result_df.loc[subject]=res
                 
 #%% save
-result_df.to_csv('../../data/Wagenmakers/SpeedAccData_filtered_for_behavioral_analyses.csv', index='Subject')
+result_df.to_csv('../../data/Wagenmakers/ANOVA_SpeedAccData_filtered_for_behavioral_analyses.csv', index='Subject')
